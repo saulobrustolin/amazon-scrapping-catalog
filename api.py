cd3915af32cf6756784546fdfdc3b63c194bf7e3
Saulo Brustolin <saulobrustolin@icloud.com>
2024-11-09 15:10:28 -0300

    att

--- api.py
+++ api.py
@@ -30,4 +30,4 @@ async def catalog(request: URLRequest):  # Extrai o JSON do corpo da requisiçã
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="localhost", port=8000)
